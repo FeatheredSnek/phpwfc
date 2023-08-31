34032c9c3de71173e091d0703efc85b85ff2237f
Dorian Mączka <dorian.maczka@gmail.com>
2023-08-31 16:55:41 +0200

    added favicon

--- render.php
+++ render.php
@@ -8,8 +8,10 @@ function embed($html) {
         <head>
             <meta charset='UTF-8'>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <meta name='theme-color' content='#56224d' />
             <title>PHP-WFC - result</title>
             <link rel='stylesheet' href='style.css'>
+            <link rel='icon' href='favicon.png' />
         </head>
 
         <body>
